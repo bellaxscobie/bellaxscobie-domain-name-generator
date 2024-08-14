e11481d179ab8d629edc1b4f30cd36049147d39b
bellaxscobie <bellaxscobie@gmail.com>
2024-08-14 21:22:18 +0000

    Project complete

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronouns = ["the", "our", "my", "another"];
+  let adjectives = ["great", "small", "radical", "wonderful", "brilliant"];
+  let nouns = ["pants", "turtle", "adventure", "hands"];
+  let doms = [".travel", ".com", ".net", ".org"];
+
+  for (let pronoun of pronouns) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let dom of doms) {
+          document.querySelector("#domainNames").innerHTML +=
+            "<h1>" + pronoun + adjective + noun + dom + "</h1>";
+        }
+      }
+    }
+  }
+  console.log();
 };
